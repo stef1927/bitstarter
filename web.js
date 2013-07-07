2514baa63b56c48f2a4bcb5a936d070f7a52bd21
Stefania Alborghetti <stefania_alborghetti@yahoo.co.uk>
2013-07-07 06:49:15 +0000

    Modified to read from index.html

--- web.js
+++ web.js
@@ -1,8 +1,11 @@
 var express = require('express');
 
+var fs = require('fs');
+
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
+  var text = fs.readFileSync("index.html").toString();
   response.send('Hello World 2!');
 });
 
